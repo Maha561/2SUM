e254447303a1a146bae16b99114bdeaaab2aa48a
Maha561 <mahalakshmianandan04@gmail.com>
2026-03-02 21:22:14 +0530

    added

---
+++ optimal.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        // 2 pointer
+
+        int left = 0;
+        int right = arr.length-1;
+
+        boolean found = false;
+
+       while(left<right){
+
+           int sum = arr[left]+arr[right];
+
+           if(sum==target){
+
+               System.out.println("yes");
+               found = true;
+               break;
+           } else if (sum<target) {
+
+               left++;
+
+           }
+           else {
+               right--;
+           }
+       }
+
+       if(found==false){
+
+           System.out.println("no");
+       }
+
+
+    }
+}
